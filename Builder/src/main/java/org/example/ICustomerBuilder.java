d87b12d9a181f4bf7cc012c88c1c8e6dd6edc1a6
gaurav-x5 <gaurav.sit.cse@gmail.com>
2023-11-14 01:44:52 +0530

    BUILDER Design Pattern

---
+++ Builder/src/main/java/org/example/ICustomerBuilder.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public interface ICustomerBuilder {
+    ICustomerBuilder firstName(String firstName);
+    ICustomerBuilder lastName(String lastName);
+    ICustomerBuilder middleName(String middleName);
+    ICustomerBuilder primaryEmail(String primaryEmail);
+    ICustomerBuilder secondaryEmail(String secondaryEmail);
+    ICustomerBuilder primaryMobileNumber(String primaryMobileNumber);
+    ICustomerBuilder secondaryMobileNumber(String secondaryMobileNumber);
+}
